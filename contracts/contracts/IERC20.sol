de0fe1c05318a0504461b8d279fbdd1f0893f2d9
StewieC <makokha.stewart21@students.dkut.ac.ke>
2025-10-23 13:53:49 +0300

    removed everything

--- contracts/contracts/IERC20.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface IERC20 {
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    function approve(address spender, uint256 amount) external returns (bool);
-}
